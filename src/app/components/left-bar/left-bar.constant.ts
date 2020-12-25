cc49fec5163b8f18826bfc95d3ffff1715e91c60
Henry Lee <henry.l@itcgroup.xyz>
2020-12-25 22:02:55 +0700

    direct UI

--- src/app/components/left-bar/left-bar.constant.ts
+++ src/app/components/left-bar/left-bar.constant.ts
@@ -1,12 +1,14 @@
 export const menu1 = [
   { title: 'Quản lý đơn hàng', iconUrl: '/assets/img/icon1.svg', url: '/' },
-  { title: 'Định tuyến', iconUrl: '/assets/img/icon2.svg', url: '/direct' },
-  { title: 'Theo dõi giao nhận', iconUrl: '/assets/img/icon3.svg', url: '/track-order' },
-  { title: 'Trò chuyện', iconUrl: '/assets/img/icon1.svg', url: '/conversation' }
+  { title: 'Đánh giá', iconUrl: '/assets/img/icon2.svg', url: '/' },
+  { title: 'Định tuyến', iconUrl: '/assets/img/icon-direct.svg', url: '/direct' },
+  { title: 'Theo dõi giao nhận', iconUrl: '/assets/img/icon1.svg', url: '/track-order' },
+  { title: 'Trò chuyện', iconUrl: '/assets/img/icon3.svg', url: '/conversation' }
 ];
 
 export const menu2 = [
   { title: 'Quản lý đơn hàng', iconUrl: '/assets/img/icon1.svg', url: '/' },
-  { title: 'Theo dõi giao nhận', iconUrl: '/assets/img/icon3.svg', url: '/track-order' },
-  { title: 'Trò chuyện', iconUrl: '/assets/img/icon1.svg', url: '/conversation' }
+  { title: 'Đánh giá', iconUrl: '/assets/img/icon2.svg', url: '/' },
+  { title: 'Theo dõi giao nhận', iconUrl: '/assets/img/icon1.svg', url: '/track-order' },
+  { title: 'Trò chuyện', iconUrl: '/assets/img/icon3.svg', url: '/conversation' }
 ];
